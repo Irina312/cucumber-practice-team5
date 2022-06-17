458e09ff2d130a8fbf84e68cda49fbdc18afd1ca
Irina312 <irina.evans@yahoo.com>
2022-06-17 18:19:50 -0400

    Added Assert class

--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -1,14 +1,18 @@
 package step_definitions;
 
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import utils.WebDriverManager;
 
 import java.util.List;
 
+import static pages.CommonPage.XPATH_TEMPLATE_TEXT_CONTAINS;
+
 public class HomeSteps {
     @Then("Verify {string} link is displayed")
     public void verifyLinkIsDisplayed(String dashboard) {
         Assert.assertTrue((WebDriverManager.isDisplayed(By.xpath(String.format(XPATH_TEMPLATE_TEXT_CONTAINS, dashboard)))));
+    }
 }
