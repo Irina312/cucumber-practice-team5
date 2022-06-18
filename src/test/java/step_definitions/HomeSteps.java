a153d443c399b21fc8f5165c1024f054640ba2d0
kalyaratR <kitty25242009@hotmail.com>
2022-06-17 22:59:51 -0400

    add new

--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -19,34 +19,25 @@ public class HomeSteps implements CommonPage {
         Assert.assertTrue((WebDriverManager.isDisplayed(By.xpath(String.format(XPATH_TEMPLATE_TEXT_CONTAINS, dashboard)))));
     }
     @Given("User click on Do's section")
-    public void user_click_on_do_s_section(String enter) {
-        WebDriverManager.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_BUTTON, enter))).click();
+    public void user_click_on_do_s_section(String btn) {
+        WebDriverManager.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_BUTTON, btn))).click();
     }
     @When("User Verify {string} section is enabled to click")
-    public void user_verify_section_is_enabled_to_click(String string) {
-
+    public void user_verify_section_is_enabled_to_click(String typ) {
+        WebDriverManager.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_BUTTON, typ))).click();
     }
     @When("User enter the value as a letter")
-    public void user_enter_the_value_as_a_letter() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_enter_the_value_as_a_letter(String addletter) {
+        WebDriverManager.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_INPUT_FIELD, addletter))).click();
     }
     @When("User enter the value as a number")
-    public void user_enter_the_value_as_a_number() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_enter_the_value_as_a_number(String addnumber) {
+        WebDriverManager.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_INPUT_FIELD, addnumber))).click();
     }
     @Then("User click on {string} button and verify is clickable")
-    public void user_click_on_button_and_verify_is_clickable(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void user_click_on_button_and_verify_is_clickable(String enter) {
+        WebDriverManager.getDriver().findElement(By.xpath(String.format(XPATH_TEMPLATE_BUTTON, enter))).click();
     }
-    @Given("User click on Don't section")
-    public void user_click_on_don_t_section() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
 
     }
 
